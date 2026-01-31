57f9cd3b075f7e33ae403c7e516c3a04af795bb1
hamza-halawa2020 <h.halawa2020@gmail.com>
2026-01-31 18:54:06 +0200

    feat: Add `FileController` and a new route to serve files from storage.

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LanguageController;
+use App\Http\Controllers\FileController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/language/{locale}', [LanguageController::class, 'switchLanguage'])->name('language.switch');
+
+Route::get('/storage/{filename}', [FileController::class, 'show'])->name('storage.file');
